c672f176e97d34d6f3832fb80e0f8b32ef401ccd
simegnew-m <simegnew.misganaw@gmail.com>
2021-11-16 08:49:52 -0800

    # 0x01-python if else loops functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,6 @@ if number < 0:
     r = number % -10
 else:
     r = number % 10
-# if the last digit is greater than 5: the string and is greater than 5
-# if the last digit is 0: the string and is 0
-# if the last digit is less than 6 and not 0: the string and is less than 6 and not 0
 print('Last digit of', number, 'is', r, end=' ')
 if r > 5:
     print('and is greater than 5')
