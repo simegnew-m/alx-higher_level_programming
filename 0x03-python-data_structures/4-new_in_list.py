51a0e4cbe3ece71921ecc7de72034bc81ac9dc21
simegnew-m <simegnew.misganaw@gmail.com>
2021-11-22 03:04:52 -0800

    # 0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list) :
+    if idx < 0 or idx >= len(copy_my_list) :
         return copy_my_list
     else:
         copy_my_list[idx] = element
