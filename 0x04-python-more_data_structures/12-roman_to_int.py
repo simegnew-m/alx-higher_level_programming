bc58fa6d704e1521148334cebccf294d5ad05599
simegnew-m <simegnew.misganaw@gmail.com>
2021-11-24 10:05:39 -0800

    # 0x04-python more data structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-   
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+    roman = {
+        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "DM": 900,
+        "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400
     }
-    num = 0
-
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+    if not isinstance(roman_string, str):
+        return 0
+    v = 0
+    c = 0
+    l = len(roman_string)
+    while c < l:
+        if c + 1 < l and roman_string[c:c + 2] in roman:
+            v += roman[roman_string[c:c + 2]]
+            c += 2
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            v += roman[roman_string[c]]
+            c += 1
+    return v
