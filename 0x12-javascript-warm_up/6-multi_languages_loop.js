69819993e40b0aedbff2dfee8891896c18633269
simegnew-m <simegnew.misganaw@gmail.com>
2022-02-28 11:41:27 -0800

    # 0x12-javascript warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-'use strict';
-let args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let arg in args) {
-  console.log(args[arg]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
