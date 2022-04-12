b4a12bcf637f29c21c8315ef24cbfcf92a50acb1
simegnew-m <simegnew.misganaw@gmail.com>
2022-04-12 10:23:35 -0700

    # 0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// JS Script
-require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+const request = require('request');
+let url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
